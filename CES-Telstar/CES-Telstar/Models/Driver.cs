1269aa702248575e2e1520f1066be4e43f744e80
Steffen Aller Jacobsen <stal@netcompany.com>
2020-09-10 13:33:47 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ CES-Telstar/CES-Telstar/Models/Driver.cs
@@ -0,0 +1,16 @@
+﻿namespace CES_Telstar.Models
+{
+    public enum Company
+    {
+        TelstarLogistics,
+        EastIndiaTrading,
+        OceanicAirlines
+    }
+
+    public class Driver
+    {
+        public int ID { get; set; }
+        public Company Company { get; set; }
+
+    }
+}
---
+++ CES-Telstar/CES-Telstar/Models/Segment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CES_Telstar.Models
+{
+    public class Segment
+    {
+        public int ID { get; set; }
+        public IEnumerable<Location> Locations { get; set; }
+        public int Time { get; set; }
+        public float Price { get; set; }
+    }
+}
