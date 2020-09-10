108e853643ead09900baa74d8124ecece3cc159d
Jonas Karlsson <jonk@netcompany.com>
2020-09-10 15:22:16 +0200

    Added DI and populated controllers

---
+++ CES-Telstar/CES-Telstar/ViewModels/DeliveryRoute.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CES_Telstar.ViewModels
+{
+    public class DeliveryRoute
+    {
+        //TODO public List<Route>
+        public double Time { get; set; }
+        public double Price { get; set; }
+        public bool HasShipping { get; set; }
+        public bool HasFlight { get; set; }
+        public bool IsRecommended { get; set; }
+    }
+}
